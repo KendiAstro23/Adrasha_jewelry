eee6a85408af8a9711ef3e7d116c0fdcf5aa4fd5
KendiAstro23 <litalakendy975@gmail.com>
2024-12-16 13:53:20 +0300

    Changes to the homepage

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 // src/components/Cart.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
